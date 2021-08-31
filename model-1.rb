0a40cae5c1fe88fc45188602ff04c6af47ff7503
Sam Iavri <sammi80134@gmail.com>
2021-08-31 13:01:02 -0600

    Add booleans

--- model-1.rb
+++ model-1.rb
@@ -2,6 +2,11 @@ plant = "pothos"
 light = "indirect"
 skill_level = "beginner"
 
-Number_of_pothos = 2
-Times_a_week_to_water= 1
-Tsp_of_food_to_feed_a_week = .5
+number_of_pothos = 2
+times_a_week_to_water= 1
+tsp_of_food_to_feed_a_week = .5
+
+pothos_is_blue = false
+pothos_is_green = true
+ceramic_pot = false
+terracotta_pot = true
